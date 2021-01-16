83d1f2158be365a7bf547ec12ef8d03eeee08e89
alrudolph <alex3r@me.com>
2021-01-16 00:05:06 -0800

    colored buttons

--- Displays/Keypad.tsx
+++ Displays/Keypad.tsx
@@ -22,34 +22,34 @@ export default function Keypad() {
   return (
     <Display>
         <Row>
-            <DarkGrayButton text="+" onTouch={() => {alert("HELLO")}} />
-            <DarkGrayButton text="+" onTouch={() => {alert("HELLO")}} />
-            <DarkGrayButton text="+" onTouch={() => {alert("HELLO")}} />
-            <DarkGrayButton text="+" onTouch={() => {alert("HELLO")}} />
+            <LightGrayButton text="AC" onTouch={() => {alert("HELLO")}} />
+            <LightGrayButton text="V" onTouch={() => {alert("HELLO")}} />
+            <LightGrayButton text="." onTouch={() => {alert("HELLO")}} />
+            <OrangeButton text="*/*" onTouch={() => {alert("HELLO")}} />
         </Row>
         <Row>
-            <DarkGrayButton text="+" onTouch={() => {alert("HELLO")}} />
-            <DarkGrayButton text="+" onTouch={() => {alert("HELLO")}} />
-            <DarkGrayButton text="+" onTouch={() => {alert("HELLO")}} />
-            <DarkGrayButton text="+" onTouch={() => {alert("HELLO")}} />
+            <DarkGrayButton text="7" onTouch={() => {alert("HELLO")}} />
+            <DarkGrayButton text="8" onTouch={() => {alert("HELLO")}} />
+            <DarkGrayButton text="9" onTouch={() => {alert("HELLO")}} />
+            <OrangeButton text="X" onTouch={() => {alert("HELLO")}} />
         </Row>
         <Row>
-            <DarkGrayButton text="+" onTouch={() => {alert("HELLO")}} />
-            <DarkGrayButton text="+" onTouch={() => {alert("HELLO")}} />
-            <DarkGrayButton text="+" onTouch={() => {alert("HELLO")}} />
-            <DarkGrayButton text="+" onTouch={() => {alert("HELLO")}} />
+            <DarkGrayButton text="4" onTouch={() => {alert("HELLO")}} />
+            <DarkGrayButton text="5" onTouch={() => {alert("HELLO")}} />
+            <DarkGrayButton text="6" onTouch={() => {alert("HELLO")}} />
+            <OrangeButton text="-" onTouch={() => {alert("HELLO")}} />
         </Row>
         <Row>
-            <DarkGrayButton text="+" onTouch={() => {alert("HELLO")}} />
-            <DarkGrayButton text="+" onTouch={() => {alert("HELLO")}} />
-            <DarkGrayButton text="+" onTouch={() => {alert("HELLO")}} />
-            <DarkGrayButton text="+" onTouch={() => {alert("HELLO")}} />
+            <DarkGrayButton text="1" onTouch={() => {alert("HELLO")}} />
+            <DarkGrayButton text="2" onTouch={() => {alert("HELLO")}} />
+            <DarkGrayButton text="3" onTouch={() => {alert("HELLO")}} />
+            <OrangeButton text="+" onTouch={() => {alert("HELLO")}} />
         </Row>
         <Row>
-            <DarkGrayButton text="+" onTouch={() => {alert("HELLO")}} />
-            <DarkGrayButton text="+" onTouch={() => {alert("HELLO")}} />
-            <DarkGrayButton text="+" onTouch={() => {alert("HELLO")}} />
-            <DarkGrayButton text="+" onTouch={() => {alert("HELLO")}} />
+            <DarkGrayButton text="0" onTouch={() => {alert("HELLO")}} />
+            <DarkGrayButton text="(" onTouch={() => {alert("HELLO")}} />
+            <DarkGrayButton text=")" onTouch={() => {alert("HELLO")}} />
+            <OrangeButton text="=" onTouch={() => {alert("HELLO")}} />
         </Row>
     </Display>
   );
