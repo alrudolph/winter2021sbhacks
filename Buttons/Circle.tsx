7348208e263315ee017a5e911d696c140010bc39
Matthew Ho <msho@ucsb.edu>
2021-01-15 23:32:53 -0800

    active opacity

--- Buttons/Circle.tsx
+++ Buttons/Circle.tsx
@@ -38,6 +38,7 @@ interface CircleInput {
 export default function Circle({ primaryColor, secondaryColor, textColor, text, onTouch }: CircleInput) {
   return (
       <Highlight
+      	  activeOpacity={0.3}
           underlayColor={secondaryColor}
           onPress = {() => {onTouch()}}  
           style = {{alignItems: "center"}}
