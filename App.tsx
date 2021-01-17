600f52f4a5ff6c8440c134b07c78790609eb0e4e
Matthew Ho <msho@ucsb.edu>
2021-01-16 17:25:37 -0800

    moved scoll to separate file

--- App.tsx
+++ App.tsx
@@ -17,169 +17,137 @@ import output from './Constants/tokens';
 import { Variables } from './Constants/Variables';
 
 import calculator, {addParenthesis} from "./Constants/calculator";
-import Current from './Displays/Current';
-import History from './Displays/History';
+
+/*
+	 import Current from './Displays/Current';
+	 import History from './Displays/History';
+ */
+
+import { Item } from './Displays/PrevList';
 
 const Body = styled.View`
-  width: 100%;
-  height: 100%;
-  background-color: ${Black};
-  padding: 2.5%;
-`
+width: 100%;
+height: 100%;
+background-color: ${Black};
+padding: 2.5%;
+display: flex;
+`;
+
 type stored = {
-  queue: Array<Types>,
-  history: string,
-  val: string
+queue: Array<Types>,
+			 history: string,
+			 val: string
 }
-
-
-const Item = ({ input, output}) => (
-  <View style={styles.item}>
-    <Text style={styles.input}>{input}</Text>
-    <View style={{flex: 1}}></View>
-    <Text style={styles.output}>{output}</Text>
-  </View>
-);
-
 
 export default function App() {
-  const [mode, setMode] = useState("num");
+	const [mode, setMode] = useState("num");
+	const [prev, setPrev] = useState([]);
 
-  const defaultState: stored = { queue: [], history: "", val: ""};
-  const [{ queue, history, val }, setExpression] = useState(defaultState);
+	const defaultState: stored = { queue: [], history: "", val: ""};
+	const [{ queue, history, val }, setExpression] = useState(defaultState);
 
-  const [prev, setPrev] = useState([]);
+	useEffect(() => {
+			const output = queue.reduce((acc, curr) => acc + curr.display, "");
+			setExpression({ queue, history: output, val});
+			}, [queue])
 
-  useEffect(() => {
-    const output = queue.reduce((acc, curr) => acc + curr.display, "");
-    setExpression({ queue, history: output, val});
-  }, [queue])
+	const appendQueue = (n: Types) => {
+		const newQ = [...queue, n];
+		setExpression({ queue: newQ, history, val });
+	}
 
-  const appendQueue = (n: Types) => {
-    const newQ = [...queue, n];
-    setExpression({ queue: newQ, history, val });
-  }
+	const append = (n: Types) => {
+		if (n.type === "digit") {
+			const lastElem = queue.length > 0 ? queue[queue.length - 1] : null;
+			if (lastElem !== null && lastElem.type == "DigitBuilder") {
+				const newQ = [...queue.slice(0, queue.length - 1), lastElem.append(n)];
+				setExpression({ queue: newQ, history, val});
+			}
+			else {
+				appendQueue(new DigitBuilder(n));
+			}
+		}
+		else if (n.type === "operator") {
+			appendQueue(n);
+		}
+		else if (n.type === "parenthesis") {
+			if (n.display === ")") {
+				setExpression({ queue: addParenthesis([...queue, n]), history, val });
+			}
+			else {
+				appendQueue(n);
+			}
+		}
+		else if (n.type === "variable") {
+			appendQueue(n);
+		}
+	}
 
-  const append = (n: Types) => {
-    if (n.type === "digit") {
-      const lastElem = queue.length > 0 ? queue[queue.length - 1] : null;
-      if (lastElem !== null && lastElem.type == "DigitBuilder") {
-        const newQ = [...queue.slice(0, queue.length - 1), lastElem.append(n)];
-        setExpression({ queue: newQ, history, val});
-      }
-      else {
-        appendQueue(new DigitBuilder(n));
-      }
-    }
-    else if (n.type === "operator") {
-      appendQueue(n);
-    }
-    else if (n.type === "parenthesis") {
-      if (n.display === ")") {
-        setExpression({ queue: addParenthesis([...queue, n]), history, val });
-      }
-      else {
-        appendQueue(n);
-      }
-    }
-    else if (n.type === "variable") {
-      appendQueue(n);
-    }
-  }
+	const clear = () => {
+		setExpression({ queue: [], history: "", val: "" })
+	}
 
-  const clear = () => {
-    setExpression({ queue: [], history: "", val: "" })
-  }
+	const evaluateQueue = () => {
+		return calculator(queue);
+	}
 
-  const evaluateQueue = () => {
-    return calculator(queue);
-  }
+	const equals = () => {
+		setExpression({ queue: addParenthesis([...queue]), history, val: trunc(evaluateQueue())});
 
-  const equals = () => {
-    setExpression({ queue: addParenthesis([...queue]), history, val: trunc(evaluateQueue())});
+		let updatedPrev = [...prev];
+		updatedPrev.push({input: history, output: trunc(evaluateQueue())});
+		setPrev(updatedPrev);
+	}
 
-    let updatedPrev = [...prev];
-    updatedPrev.push({input: history, output: trunc(evaluateQueue())});
-    setPrev(updatedPrev);
-  }
-  
-  const showVar = () => {
-    setMode("var");
-  }
+	const showVar = () => {
+		setMode("var");
+	}
 
-  const back = () => {
-    setMode("num");
-  }
+	const back = () => {
+		setMode("num");
+	}
 
-const renderItem = ({ item, output}) => (
-    <Item input={item.input} output={item.output}/>
-  );
- 
-const scrollViewRef = useRef();
+	const renderItem = ({ item, output}) => (
+			<Item input={item.input} output={item.output}/>
+			);
 
-  return (
-    <Body>
-      <View style={{height: "5%"}}>
-      <ScrollView ref={scrollViewRef}
-      onContentSizeChange={()=> scrollViewRef.current.scrollToEnd({animated: true})}>
-      <FlatList
-        data={prev}
-        renderItem={renderItem}
-      />
-      </ScrollView>
-      </View>
-      <Variables>
-      	<View style={{height: "30%"}}>
-	<NumberDisplay history={history} val={val} />
-	{/*
-	<History history={history} />
-	<Current value={val} />
-	*/}
-	</View>
-        {mode === "num" ? 
-        (<Keypad 
-            append={(input: Types) => { append(input); }}
-            clear={() => { clear(); }}
-            equals={() => { equals(); }}
-              showVar={() => { showVar(); }}
-          />
-        ) : (
-          <VarPad 
-            append={(input: Types) => { append(input); }}
-            back={() => { back(); }}
-            currVal={evaluateQueue()}
-            currDisplay={val}
-          />
-        )}
-      </Variables>
-    </Body>
-  );
+	const scrollViewRef = useRef();
+
+	return (
+			<Body>
+				<View style={{height: "5%"}}>
+					<ScrollView ref={scrollViewRef}
+						onContentSizeChange={()=> scrollViewRef.current.scrollToEnd({animated: true})}>
+						<FlatList
+							data={prev}
+							renderItem={renderItem}
+						/>
+					</ScrollView>
+				</View>
+				<Variables>
+					<View style={{height: "30%"}}>
+						<NumberDisplay history={history} val={val} />
+			{/*
+					<History history={history} />
+					<Current value={val} />
+				*/}
+					</View>
+			{mode === "num" ? 
+			(<Keypad 
+			 	append={(input: Types) => { append(input); }}
+			 	clear={() => { clear(); }}
+			 	equals={() => { equals(); }}
+			 	showVar={() => { showVar(); }}
+			 />
+			) : (
+			<VarPad 
+				append={(input: Types) => { append(input); }}
+				back={() => { back(); }}
+				currVal={evaluateQueue()}
+				currDisplay={val}
+			/>
+			)}
+				</Variables>
+		</Body>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    height: "25%"
-  },
-  item: {
-    backgroundColor: Black,
-    //backgroundColor: "#40b1ed",
-    paddingTop: 5,
-    paddingLeft: "5%",
-    paddingRight: "5%",
-    marginVertical: 5,
-    marginHorizontal: 0,
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "flex-end"
-  },
-  input: {
-    fontSize: 24,
-    color: White
-  },
-  output: {
-    fontSize: 24,
-    color: White,
-    justifySelf: "flex-end",
-   }
-});
---
+++ Displays/PrevList.tsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { ScrollView, SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from 'react-native';
+import { White, Black, LightGray } from '../Constants/Palette'
+
+
+export const Item = ({ input, output}) => (
+  <View style={prevStyles.item}>
+    <Text style={prevStyles.input}>{input}</Text>
+    <View style={{flex: 1}}></View>
+    <Text style={prevStyles.output}>{output}</Text>
+  </View>
+);
+
+export const prevStyles = StyleSheet.create({
+  item: {
+    backgroundColor: Black,
+    //backgroundColor: "#40b1ed",
+    paddingTop: 5,
+    paddingLeft: "5%",
+    paddingRight: "5%",
+    marginVertical: 5,
+    marginHorizontal: 0,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-end"
+  },
+  input: {
+    fontSize: 24,
+    color: White
+  },
+  output: {
+    fontSize: 24,
+    color: White,
+    justifySelf: "flex-end",
+   }
+});
